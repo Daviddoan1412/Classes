65b39c20ed5617451d0dcf5b0f566c85e887d7b1
david_doan <doanchuong14122k@gmail.com>
2020-07-12 23:59:46 +0700

    code

---
+++ assets/function.js
@@ -0,0 +1,7 @@
+var open_right = document.getElementById("open-right");
+var button = document.getElementById("button-outside");
+
+function demo() {
+    open_right.classList.toggle("show");
+    button.classList.toggle("show1");
+}
